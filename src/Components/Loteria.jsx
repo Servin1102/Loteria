6d166bf20d550f9e891a7351fa317867ed775d3d
Juan Martinez <juanmaa1102@gmail.com>
2022-06-28 06:45:20 -0500

    changes

--- src/Components/Loteria.jsx
+++ src/Components/Loteria.jsx
@@ -1,33 +1,38 @@
-import React from 'react';
-import { useState } from 'react'
-import loteriacard from "../loteriacard.json"
+import React from "react";
+import { useState } from "react";
+import loteriacard from "../loteriacard.json";
 
 const Loteria = () => {
-    const random = () => Math.floor(Math.random() * loteriacard.length);
+  const random = () => Math.floor(Math.random() * loteriacard.length);
 
-    const [card, setcard] = useState(0)
+  const [card, setcard] = useState(0);
 
-    const clic = () =>{
-        setcard(random());
-    }
-    const reiniciar = () =>{
-      alert("Bienvenido a Loteria by Servin")
-      document.location.reload()
-    }
-     
-    return (
-        <div className="App-header">
+  const clic = () => {
+    setcard(random());
+    console.log(card);
+  };
+  const reiniciar = () => {
+    alert("Bienvenido a Loteria by Servin");
+    document.location.reload();
+  };
+
+  return (
+    <div className="App-header">
       <div className="cardlote">
-        <img src={loteriacard[card].image} className="imglote"  alt="" />
-        <h3 style={{textTransform: "uppercase"}}>{loteriacard[card].name}</h3>
+        <img src={loteriacard[card].image} className="imglote" alt="" />
+        <h3 style={{ textTransform: "uppercase" }}>{loteriacard[card].name}</h3>
         <h6>"{loteriacard[card].phrase}"</h6>
       </div>
       <div>
-        <button onClick={clic}><i className="fa-solid fa-circle-right"></i></button>
-        <button onClick={reiniciar}><i class="fa-solid fa-arrows-rotate"></i></button>
+        <button onClick={clic}>
+          <i className="fa-solid fa-circle-right"></i>
+        </button>
+        <button onClick={reiniciar}>
+          <i className="fa-solid fa-arrows-rotate"></i>
+        </button>
       </div>
-      </div>
-    );
+    </div>
+  );
 };
 
 export default Loteria;
